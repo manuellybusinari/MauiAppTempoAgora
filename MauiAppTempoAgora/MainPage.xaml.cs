6b17e46e2cf253d79aeea99484e9721d55e1f3fb
manuellybusinari <manuellybusinari@outlook.com>
2025-09-30 13:15:19 -0300

    30 09 2025

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -6,13 +6,12 @@ namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
            try
             {
@@ -52,6 +51,71 @@ namespace MauiAppTempoAgora
 
                 await DisplayAlert("Ops", ex.Message, "OK");
 
+            }
+        }
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                        GeolocationAccuracy.Medium, 
+                        TimeSpan.FromSeconds(10) // Tenta Procurar por 10 segundos 
+                    );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request); //Geolocation.Deafult: Acesso ao GPS do disositivo //GetLocationAsync: Tenta pegar a a localização atual//Location? local: guarda os dados, pode ser 'null' se não conseguir 
+
+                if (local != null) // Se o local não for nulo 
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n " +
+                                        $"Longitude: {local.Longitude} \n";
+
+                    lbl_coords.Text = local_disp; // Indo atrás da localização
+
+                    // Nome da cidade que está nas coordenadas 
+                    GetCidade(local.Latitude, local.Longitude);
+                }
+                else
+                {
+                    lbl_coords.Text="Nenhuma loclaização";
+                }
+
+            }
+            catch (FeatureNotSupportedException fnsEx) // Não tem o suporte para a localização 
+            {
+                await DisplayAlert("Erro: Dispositivo não suporta", fnsEx.Message, "OK");
+
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão da Localização", pEx.Message, "OK"); 
+            }
+            catch(Exception ex) //catch genérico
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+
+        }
+
+        private async void GetCidade(double lat, double lon) // retorna preenchimento automático qual cidade eu estou 
+        { //Habilite o try/catch para não crachear!!! - Habilite oo Token em depuração Windows
+            try { 
+            // Conceito Placemark 
+            IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+            
+            Placemark? place = places.FirstOrDefault();  
+
+            if (place != null)
+            {
+                txt_cidade.Text = place.Locality;
+            }
+            } catch (Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da cidade", ex.Message, "OK");
             }
         }
     }
